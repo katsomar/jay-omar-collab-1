2b22bbfd6566f39f4d79a922ea82f806cc81deaa
raj-jadhav <jadxur@gmail.com>
2024-11-28 13:33:51 +0300

    Commit4-task5: Added a prog for Total-revenue calculation and compiled it and created an exe for it

--- task5.c
+++ task5.c
@@ -15,15 +15,27 @@ int main(){
     int monthly_salary = hourly_wage * hours_worked;
     printf("The monthly salary of the employee = UGX %d\n",monthly_salary);    
     */
-    
+    /*
     //Net salary calculation
     int gross_salary = 5000;
     float tax_rate = 0.1;
     int net_salary = gross_salary - (tax_rate * gross_salary);
 
     printf("The net salary = UGX %d\n",net_salary);
+    */
+
+    //Total Revenue Calculation
+    int price1 = 3000;
+    int price2 = 4500;
+    int price3 = 6000;
     
-    
+    int qty1 = 100;
+    int qty2 = 150;
+    int qty3 = 200;
+
+    int total_revenue = (price1 * qty1) + (price2 * qty2) + (price3 * qty3);
+
+    printf("The total revenue = UGX %d\n",total_revenue);
    
 
 return 0;
