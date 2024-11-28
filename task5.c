0158cad0ebb06fc12b2b25b586156362fbd3f66c
raj-jadhav <jadxur@gmail.com>
2024-11-28 12:44:21 +0300

    Commit1-task5: Added a prog for Total cost calculation and compiled task5 file to create an exe for Total-cost

---
+++ task5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int unit_price = 1500;
+    int quantity = 2000;
+    int total_cost = unit_price * quantity;
+    printf("The total cost of items = UGX %d\n",total_cost);
+
+return 0;
+}
