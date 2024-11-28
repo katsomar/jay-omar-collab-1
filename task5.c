f1581b629273678b526f4cad3cb5de224be12faf
raj-jadhav <jadxur@gmail.com>
2024-11-28 13:28:03 +0300

    Commit3-task5: Added a prog for Net salary calculation and compiled it and created an exe for it

--- task5.c
+++ task5.c
@@ -8,12 +8,23 @@ int main(){
     int total_cost = unit_price * quantity;
     printf("The total cost of items = UGX %d\n",total_cost);
     */
-
+   /*
     //Monthly Salary Calculation
     int hourly_wage = 2500;
     int hours_worked = 16000;
     int monthly_salary = hourly_wage * hours_worked;
     printf("The monthly salary of the employee = UGX %d\n",monthly_salary);    
+    */
+    
+    //Net salary calculation
+    int gross_salary = 5000;
+    float tax_rate = 0.1;
+    int net_salary = gross_salary - (tax_rate * gross_salary);
+
+    printf("The net salary = UGX %d\n",net_salary);
+    
+    
+   
 
 return 0;
 }
