fd1efddb36225d40f78ba87e9f06ee56106b6cac
raj-jadhav <jadxur@gmail.com>
2024-11-28 13:05:39 +0300

    Commit2-task5:Added a program for the monthly salary and compiled and created an exe for Monthly-salary

--- task5.c
+++ task5.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 int main(){
+    /*
+    //Total cost calculation
     int unit_price = 1500;
     int quantity = 2000;
     int total_cost = unit_price * quantity;
     printf("The total cost of items = UGX %d\n",total_cost);
+    */
+
+    //Monthly Salary Calculation
+    int hourly_wage = 2500;
+    int hours_worked = 16000;
+    int monthly_salary = hourly_wage * hours_worked;
+    printf("The monthly salary of the employee = UGX %d\n",monthly_salary);    
 
 return 0;
 }
