f7f85df084cd016291d85b6c1d5b4f6e74b2f417
raj-jadhav <jadxur@gmail.com>
2024-12-12 12:30:07 +0300

    Added a program that uses a function to calc & print the Employee salary after a bonus amount is added

--- task6.1.c
+++ task6.1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 void discounted_price(int price, float discount_rate);
 void tax_amount(int price , float tax_rate);
-
+void total_salary(int base_salary , float bonus_rate);
 int main(){
         discounted_price(25000,0.2);
         tax_amount(25000, 20);
+        total_salary(500000,0.5); 
 return 0;    
 }
 
@@ -18,3 +19,8 @@ void tax_amount(int price , float tax_rate){
     float tax_amount = (int)(price * (tax_rate/100));
     printf("The taxed amount = UGX %.1f\n",tax_amount);
 }
+//13.Employee Salary After Bonus
+void total_salary(int base_salary , float bonus_rate){
+    int total_salary = base_salary + (bonus_rate * base_salary);
+    printf("The Total salary is UGX%d\n",total_salary);
+} 
