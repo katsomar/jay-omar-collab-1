7933df689a157638b94d71f1781e9a60a084c9ef
raj-jadhav <jadxur@gmail.com>
2024-12-12 13:04:03 +0300

    Added a program that uses a function to calc & print the Profit & loss

--- task6.1.c
+++ task6.1.c
@@ -2,10 +2,16 @@
 void discounted_price(int price, float discount_rate);
 void tax_amount(int price , float tax_rate);
 void total_salary(int base_salary , float bonus_rate);
+void profit_loss(int selling_price, int cost_price);
 int main(){
+        //11
         discounted_price(25000,0.2);
+        //12
         tax_amount(25000, 20);
+        //13
         total_salary(500000,0.5); 
+        //14
+        profit_loss(20000,15000);
 return 0;    
 }
 
@@ -22,5 +28,17 @@ void tax_amount(int price , float tax_rate){
 //13.Employee Salary After Bonus
 void total_salary(int base_salary , float bonus_rate){
     int total_salary = base_salary + (bonus_rate * base_salary);
-    printf("The Total salary is UGX%d\n",total_salary);
+    printf("The Total salary is UGX %d\n",total_salary);
 } 
+//14.Profit or Loss
+void profit_loss(int selling_price, int cost_price) {
+    int profit_loss = selling_price - cost_price;
+
+    if (selling_price > cost_price) {
+        printf("The profit is UGX %d\n", profit_loss);
+    } else if (selling_price < cost_price) {
+        printf("The loss is UGX %d\n", -profit_loss); // Display absolute value for loss
+    } else {
+        printf("No profit, no loss.\n");
+    }
+}
